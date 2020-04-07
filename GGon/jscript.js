bf6129e6912b087ff21059dd8472ca6678bdce95
TuanPhamHP <phamanhtuan290896@gmail.com>
2020-04-07 17:40:31 +0700

    reponsive
    reponsive

--- GGon/jscript.js
+++ GGon/jscript.js
@@ -1,11 +1,30 @@
 $(document).ready(function () {
-    $(window).on('scroll',()=>{
+    $(window).on('scroll load',()=>{
+        var bannerh = $('.banner').height();
+        var usecases = $('.use').height();
         var offset = $(window).scrollTop();
-        if(offset >= 160){
+        var wd = $(window).width();
+        var headerh = $('.header-banner__content').height
+        $('.ecommerce').css('margin-top',usecases -32 + 'px')
+        if(offset >= bannerh|| wd <=480){
+            $('.header-banner__text').css('margin-top','70px')
             $('.navbar--wrapper').addClass('sticky')
+            
         }else{
             $('.navbar--wrapper').removeClass('sticky')
+        };
+        if(wd <=576){
+            $('.ads-content__left').addClass('order-1')
+        }else{
         }
     })
-
+    $('.header-toggler--mod').attr('data-target','');
+    $('.blog-block').on('click',()=>{
+        $(this).find('.txt-toggle').toggle();
+    })
+    $('.header-toggler--mod').on('click',()=>{
+        $('.navbar-toggler-icon--mod').toggleClass('tg2');
+        $('#my-nav').toggleClass('move');
+        $('.blur').toggle();
+    })
 });
