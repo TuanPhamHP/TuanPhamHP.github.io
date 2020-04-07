6cf267ccf91ed6a6cf807e514fd4d0b36ce5e3d9
TuanPhamHP <phamanhtuan290896@gmail.com>
2020-04-07 18:03:40 +0700

    pushhhhhhhhhhhhhhhh
    pushhhhhhhhhhhhhhhh

--- GGon/jscript.js
+++ GGon/jscript.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-    $(window).on('scroll load',()=>{
-        var bannerh = $('.banner').height();
-        var usecases = $('.use').height();
-        var offset = $(window).scrollTop();
-        var wd = $(window).width();
-        var headerh = $('.header-banner__content').height
-        $('.ecommerce').css('margin-top',usecases -32 + 'px')
+    var offset = $(window).scrollTop();
+    var wd = $(window).width();
+    var headerh = $('.header-banner__content').height
+    var bannerh = $('.banner').height();
+    var usecases = $('.use').height();
+    $(window).on('scroll',()=>{
         if(offset >= bannerh|| wd <=480){
             $('.header-banner__text').css('margin-top','70px')
             $('.navbar--wrapper').addClass('sticky')
@@ -13,18 +12,24 @@ $(document).ready(function () {
         }else{
             $('.navbar--wrapper').removeClass('sticky')
         };
-        if(wd <=576){
-            $('.ads-content__left').addClass('order-1')
-        }else{
-        }
     })
+    
+
+$(window).on('load',()=>{
+    if(wd <=576){
+        $('.ads-content__left').addClass('order-1')
+    }else{
+    }
+    $('.ecommerce').css('margin-top',usecases -32 + 'px')
     $('.header-toggler--mod').attr('data-target','');
-    $('.blog-block').on('click',()=>{
-        $(this).find('.txt-toggle').toggle();
-    })
-    $('.header-toggler--mod').on('click',()=>{
-        $('.navbar-toggler-icon--mod').toggleClass('tg2');
-        $('#my-nav').toggleClass('move');
-        $('.blur').toggle();
-    })
+        $('.blog-block').on('click',()=>{
+            $(this).find('.txt-toggle').toggle();
+        })
+        $('.header-toggler--mod').on('click',()=>{
+            $('.navbar-toggler-icon--mod').toggleClass('tg2');
+            $('#my-nav').toggleClass('move');
+            $('.blur').toggle();
+        })
+})
 });
+
