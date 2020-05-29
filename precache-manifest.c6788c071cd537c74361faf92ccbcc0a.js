9dc950fa23a02cba9d1fbf83b5412897cd77886f
TuanPhamHP <phamanhtuan290896@gmail.com>
2020-05-29 20:09:21 +0700

    Updates

---
+++ precache-manifest.c6788c071cd537c74361faf92ccbcc0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b22d52cc9675ed7bcdc23af16baa4d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ef0208317187fca22b44",
+    "url": "/react-app/static/css/main.ad12317c.chunk.css"
+  },
+  {
+    "revision": "cf790074fc073b16da18",
+    "url": "/react-app/static/js/2.d9b7eda4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.d9b7eda4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0208317187fca22b44",
+    "url": "/react-app/static/js/main.623dd7ed.chunk.js"
+  },
+  {
+    "revision": "f5c5ab8039ee66b00502",
+    "url": "/react-app/static/js/runtime-main.739e3c52.js"
+  }
+]);
