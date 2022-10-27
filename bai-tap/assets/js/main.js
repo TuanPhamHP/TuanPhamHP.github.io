8acbed2d90c7e8f23d5a4f033afc83bce193d2a6
TuanPhamHP <phamanhtuan290896@gmail.com>
2022-10-27 15:03:35 +0700

    update: push ls

--- bai-tap/assets/js/main.js
+++ bai-tap/assets/js/main.js
@@ -1,3 +1,3 @@
-// document.addEventListener('readystatechange', () => {
-// 	document.body.innerHTML = '<h1>Trang tạm đóng để chuẩn bị kiểm tra. Anh em quay lại sau nhé. Ôn bài cho kỹ nha</h1>';
-// });
+document.addEventListener('readystatechange', () => {
+	document.body.innerHTML = '<h1>Trang tạm đóng để chuẩn bị kiểm tra. Anh em quay lại sau nhé. Ôn bài cho kỹ nha</h1>';
+});
